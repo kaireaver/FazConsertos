73ca9691bc570fa45629a0c6757c2d9a4cb4484d
Raposo <theraposofox@gmail.com>
2017-07-01 16:37:01 -0300

    GATOS

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.h2.jdbcx.JdbcDataSource;
+
 import java.sql.*;
 
 /**
@@ -15,8 +17,13 @@ public class Database {
     public static void Connection () throws SQLException {
         Connection conn = null;
         try {
+            JdbcDataSource ds = new JdbcDataSource();
             Class.forName("org.h2.Driver");
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            //conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            ds.setURL(URL);
+            ds.setUser("sa");
+            ds.setPassword("sa");
+            conn = ds.getConnection();
             String query  = "SELECT * FROM TBL";
             PreparedStatement pps = conn.prepareStatement(query);
             ResultSet rs = pps.executeQuery();
