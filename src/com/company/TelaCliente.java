640146e4c325ca13c564e140819d8fe49e4552ba
deivisdede <deivisondasilva96@gmail.com>
2017-07-01 18:11:17 -0300

    bora que o maluco tá doente

--- src/com/company/TelaCliente.java
+++ src/com/company/TelaCliente.java
@@ -2,23 +2,38 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TelaCliente extends JFrame {
+public class TelaCliente extends JFrame implements ActionListener {
 
     private Container fClientes;
     private JPanel pClientes;
-    final private JLabel lNome;
-    final private JLabel lCPF;
-    final private JLabel lTelefone;
-    final private JButton bOk;
-    final private JButton bCancel;
-    private JTextField tNome;
-    private JTextField tCPF;
-    private JTextField tTelefone;
-    Box boNome;
-    Box boCPF;
-    Box boTelefone;
+
     Box boCadastro;
+        Box boNome;
+            final private JLabel lNome;
+            private JTextField tNome;
+        Box boCPF;
+            final private JLabel lCPF;
+            private JTextField tCPF;
+        Box boTelefone;
+            final private JLabel lTelefone;
+            private JTextField tTelefone;
+        final private JButton bOk;
+        final private JButton bCancel;
+
+        Box boRG;
+            private JTextField tRG;
+            private JLabel lRG;
+        Box boEmail;
+            private JTextField tEmail;
+            private JLabel lEmail;
+        Box boEndereco;
+            private JTextField tEndereco;
+            private JLabel lEndereco;
+        Box boDataNascimento;
+            private JTextField tDia, tMes, tAno;
+            private JLabel lDataNascimento;
 
 
     public TelaCliente()
@@ -27,22 +42,24 @@ public class TelaCliente extends JFrame {
         pClientes = new JPanel();
 
         boNome = Box.createHorizontalBox();
-        tNome = new JTextField(40);
-        lNome = new JLabel("Nome: ");
-        boNome.add(lNome);
-        boNome.add(tNome);
+            tNome = new JTextField(40);
+            lNome = new JLabel("Nome: ");
+            boNome.add(lNome);
+            boNome.add(tNome);
 
         boCPF = Box.createHorizontalBox();
-        tCPF = new JTextField(11);
-        lCPF = new JLabel("CPF - apenas números: ");
-        boCPF.add(lCPF);
-        boCPF.add(tCPF);
+            tCPF = new JTextField(11);
+            lCPF = new JLabel("CPF - apenas números: ");
+            boCPF.add(lCPF);
+            boCPF.add(tCPF);
 
         boTelefone = Box.createHorizontalBox();
-        tTelefone = new JTextField(12);
-        lTelefone = new JLabel("Telefone + DDD: ");
-        boTelefone.add(lTelefone);
-        boTelefone.add(tTelefone);
+            tTelefone = new JTextField(12);
+            lTelefone = new JLabel("Telefone + DDD: ");
+            boTelefone.add(lTelefone);
+            boTelefone.add(tTelefone);
+
+
 
         bOk = new JButton("OK");
         bCancel = new JButton("Cancelar");
@@ -52,24 +69,74 @@ public class TelaCliente extends JFrame {
         boCadastro.add(boCPF);
         boCadastro.add(boTelefone);
 
-        pClientes.add(boCadastro);
-        pClientes.add(bOk);
-        pClientes.add(bCancel);
-        fClientes.add(pClientes);
-
-        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.setSize(500,132);
-        this.setResizable(false);
-        this.setVisible(true);
+        atualizaCadastro(132);
     }
 
     public void actionPerformed(ActionEvent event) {
         if(event.getSource() == bOk) {
-            Cliente cliente = new Cliente(tNome.getText(), Long.parseLong(tCPF.getText()), Integer.parseInt(tTelefone.getText()));
+            System.out.print("OK então");
+            //Cliente cliente = new Cliente(tNome.getText(), Long.parseLong(tCPF.getText()), Integer.parseInt(tTelefone.getText()));
+            cadastraNovoCliente();
         }
 
         else if(event.getSource() == bCancel) {
             // ABRIR TELA CLIENTES
         }
+    }
+
+    public void cadastraNovoCliente()
+    {
+        boRG = Box.createHorizontalBox();
+            tRG= new JTextField(12);
+            lRG = new JLabel("Registro Geral: ");
+            boRG.add(lRG);
+            boRG.add(tRG);
+
+        boEmail = Box.createHorizontalBox();
+            tEmail = new JTextField(40);
+            lEmail = new JLabel("E-mail: ");
+            boEmail.add(lEmail);
+            boEmail.add(tEmail);
+
+        boEndereco = Box.createHorizontalBox();
+            tEndereco = new JTextField(40);
+            lEndereco = new JLabel("Endereço: ");
+            boEndereco.add(lEndereco);
+            boEndereco.add(tEndereco);
+
+        boDataNascimento = Box.createHorizontalBox();
+            tDia = new JTextField(2);
+            tMes = new JTextField(2);
+            tAno = new JTextField(2);
+            lDataNascimento = new JLabel("Registro Geral: ");
+            boDataNascimento.add(lDataNascimento);
+            boDataNascimento.add(tDia);
+            boDataNascimento.add(new JLabel("/"));
+            boDataNascimento.add(tMes);
+            boDataNascimento.add(new JLabel("/"));
+            boDataNascimento.add(tAno);
+
+        boCadastro.add(boRG);
+        boCadastro.add(boEmail);
+        boCadastro.add(boEndereco);
+        boCadastro.add(boDataNascimento);
+
+        this.atualizaCadastro( 200);
+    }
+
+    public void atualizaCadastro(int tam)
+    {
+        pClientes.remove(boCadastro);
+          pClientes.add(boCadastro);
+        pClientes.remove(bOk);
+            pClientes.add(bOk);
+        pClientes.remove(bCancel);
+            pClientes.add(bCancel);
+        fClientes.add(pClientes);
+
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setSize(500, tam);
+        this.setResizable(false);
+        this.setVisible(true);
     }
 }
