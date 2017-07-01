53d5121be097d7f92fdecb12eccd0cd920aacbe4
Mateus Hora <mateusdhora@gmail.com>
2017-07-01 15:58:20 -0300

    Adicionado arquivo da TelaInicial.

---
+++ src/com/company/TelaInicial.java
@@ -0,0 +1,12 @@
+package com.company;
+import javax.swing.*;
+
+public class TelaInicial {
+    private JButton bTecnicos;
+    private JButton bClientes;
+
+    public TelaInicial() {
+
+    }
+
+}
