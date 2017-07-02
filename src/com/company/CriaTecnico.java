8998ec7a99a92be1dc11c1d4c8a6ec0d9fd5b8e8
EdVillani <ed.vily@gmail.com>
2017-07-01 21:34:21 -0300

    bora que o maluco tá doente

--- src/com/company/CriaTecnico.java
+++ src/com/company/CriaTecnico.java
@@ -9,9 +9,9 @@ import java.awt.image.ImageObserver;
  * Created by edvil on 01/07/2017.
  */
 public class CriaTecnico extends JFrame{
-    private JLabel[] nome_campos;
-    private  JTextField[] campos;
-    private String[] nomes = {"Nome", "Email", "Telefone","Habilidade"};
+    private JLabel[] jlCampos;
+    private  JTextField[] tCampos;
+    private String[] jlsCampos = {"Nome", "Email", "Telefone","Habilidade"};
 
     private Container[] boxCampos;
     private  Container boxSuper;
@@ -19,15 +19,19 @@ public class CriaTecnico extends JFrame{
     private BorderLayout layout;
 
     private JComboBox cbHabilidades;
-    private String[] habilidades = {"Tecnico", "Mecanico", "Garota de Programa", "Cozinheiro"};
+    private String[] cbsHabilidades = {"Tecnico", "Mecanico", "Garota de Programa", "Cozinheiro"};
+
+    private JButton[] bCriaTecnico;
+    private String[] bsCriaTecnico = {"Criar", "Cancelar"};
 
     public CriaTecnico(){
         super("Insira suas informações");
 
-        nome_campos = new JLabel[nomes.length];
-        campos = new JTextField[nomes.length-1];
-        boxCampos = new Container[nomes.length];
-        cbHabilidades = new JComboBox(habilidades);
+        jlCampos = new JLabel[jlsCampos.length];
+        tCampos = new JTextField[jlsCampos.length-1];
+        boxCampos = new Container[jlsCampos.length+1];
+        cbHabilidades = new JComboBox(cbsHabilidades);
+        bCriaTecnico = new JButton[bsCriaTecnico.length];
 
         boxSuper = Box.createVerticalBox();
         Container container = getContentPane();
@@ -35,23 +39,31 @@ public class CriaTecnico extends JFrame{
         layout = new BorderLayout(25,25);
         container.add(this.boxSuper);
 
-        for(int i = 0; i< nomes.length-1;i++){
-            nome_campos[i] = new JLabel(nomes[i]);
-            campos[i] = new JTextField(5);
+        for(int i = 0; i< jlsCampos.length-1;i++){
+            jlCampos[i] = new JLabel(jlsCampos[i]);
+            tCampos[i] = new JTextField(5);
             boxCampos[i] = Box.createHorizontalBox();
 
-            boxCampos[i].add(nome_campos[i]);
-            boxCampos[i].add(campos[i]);
+            boxCampos[i].add(jlCampos[i]);
+            boxCampos[i].add(tCampos[i]);
             boxSuper.add(boxCampos[i]);
 
 
         }
+
         boxCampos[3] = Box.createHorizontalBox();
-        nome_campos[3] = new JLabel(nomes[3]);
-        boxCampos[3].add(nome_campos[3]);
+        jlCampos[3] = new JLabel(jlsCampos[3]);
+        boxCampos[3].add(jlCampos[3]);
         boxCampos[3].add(cbHabilidades);
         boxSuper.add(boxCampos[3]);
 
+        boxCampos[4] = Box.createHorizontalBox();
+        boxSuper.add(boxCampos[4]);
+
+        for(int i=0;i<bsCriaTecnico.length;i++){
+            bCriaTecnico[i] = new JButton(bsCriaTecnico[i]);
+            boxCampos[4].add(bCriaTecnico[i]);
+        }
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setSize(500,150);
